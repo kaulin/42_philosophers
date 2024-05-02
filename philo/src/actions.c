5c68ca057857c3a815fe2c349c2371852073ae56
Jaakko Junttila <jajuntti@student.hive.fi>
2024-05-02 10:25:31 +0300

    Moves eat counter incrementing to after eating :)

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:44:52 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/05/01 15:18:41 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/05/02 09:30:34 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	print_status(t_philo *philo, char *msg)
 
 	pthread_mutex_lock(philo->limiter);
 	timestamp = get_time_since(philo->data->start_time);
-	printf("%zums %d [%d] %s\n", timestamp, philo->id, philo->meal_count, msg);
+	printf("%zums %d %s\n", timestamp, philo->id, msg);
 	pthread_mutex_unlock(philo->limiter);
 }
 
@@ -52,10 +52,12 @@ void	eat(t_philo *philo)
 	{
 		pthread_mutex_lock(philo->limiter);
 		philo->last_meal = get_time();
-		philo->meal_count++;
 		pthread_mutex_unlock(philo->limiter);
 		print_status(philo, "is eating");
 		time_travel(philo->data->eat_time);
+		pthread_mutex_lock(philo->limiter);
+		philo->meal_count++;
+		pthread_mutex_unlock(philo->limiter);
 		release_forks(philo);
 	}
 }
