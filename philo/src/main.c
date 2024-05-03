0f3ed2ee4f1c24ab856e32240059dc197156e49d
Jaakko Junttila <jajuntti@student.hive.fi>
2024-05-03 14:21:37 +0300

    Adds files missed due to erroneous gitignore

---
+++ philo/src/main.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 12:43:42 by jajuntti          #+#    #+#             */
+/*   Updated: 2024/05/03 13:50:15 by jajuntti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+Prints a usage message specifying the required arguments.
+*/
+static int	usage(void)
+{
+	printf("usage: ./philo number_of_philosophers time_to_die time_to_eat");
+	printf(" /time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
+	return (2);
+}
+
+/*
+Prints "Error: " followed by the message stored in data->error. Then cleans 
+the data struct and returns KO (1).
+*/
+static int	fail(t_data *data)
+{
+	printf("Error: %s\n", data->error);
+	clean_data(data);
+	return (KO);
+}
+
+/*
+Checks the hungry_ones array of integers to see if all the philosophers have 
+been fed enough times already. If a hungry philosopher is encountered, returns 
+KO (1), else returns OK (1).
+*/
+static int	all_fed(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->seats)
+	{
+		if (data->hungry_ones[i])
+			return (KO);
+		i++;
+	}
+	return (OK);
+}
+
+/*
+The monitor loop iterates through the philosopher structs until:
+A. All philosophers have eaten at least the number of meals set by the 
+optional 5th program argument.
+B. The time between meals of one philosopher surpasses the alloted die_time.
+*/
+static void	monitor(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->alive_n_hungry)
+	{
+		if (!all_fed(data))
+		{
+			pthread_mutex_lock(data->limiter);
+			data->alive_n_hungry = 0;
+			pthread_mutex_unlock(data->limiter);
+			break ;
+		}
+		if (get_time_since(data->philos[i].last_meal) > (size_t)data->die_time)
+		{
+			pthread_mutex_lock(data->limiter);
+			data->alive_n_hungry = 0;
+			pthread_mutex_unlock(data->limiter);
+			print_status(&data->philos[i], "died");
+			break ;
+		}
+		if (++i == data->seats)
+			i = 0;
+	}
+}
+
+/*
+Checks the number of arguments. Initialises data. Starts the philosopher 
+threads. Starts monitor loop until end conditions are met. Joins threads. 
+Cleans data. Errors are handled after each step.
+*/
+int	main(int argc, char *argv[])
+{
+	t_data	data;
+
+	if (argc < 5 || argc > 6)
+		return (usage());
+	if (init_data(argc, argv, &data))
+		return (fail(&data));
+	if (start_threads(&data))
+		return (fail(&data));
+	monitor(&data);
+	if (join_threads(&data))
+		return (fail(&data));
+	clean_data(&data);
+	return (OK);
+}
---
+++ philo/src/threads.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 13:46:30 by jajuntti          #+#    #+#             */
+/*   Updated: 2024/05/03 14:18:48 by jajuntti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+Philosophers eat, sleep and think. To prevent deadlocks, even philosophers 
+first think and wait for 10ms before trying to grab their left fork. Odd 
+philosophers grab their forks right away (right one first) and then start 
+eating. This delay and alternate order prevents deadlocks.
+*/
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = arg;
+	if (philo->id % 2 == 0)
+	{
+		think(philo);
+		time_travel(10);
+		grab_fork(philo, philo->lfork);
+		grab_fork(philo, philo->rfork);
+	}
+	else
+	{
+		grab_fork(philo, philo->rfork);
+		grab_fork(philo, philo->lfork);
+	}
+	while (philo->data->alive_n_hungry)
+	{
+		if (philo->data->alive_n_hungry)
+			eat(philo);
+		if (philo->data->alive_n_hungry)
+			nap(philo);
+		if (philo->data->alive_n_hungry)
+			think(philo);
+	}
+	return (NULL);
+}
+
+/*
+Philosophers eat, sleep and think. In the case of only one philosopher, that 
+one is aware that they only have access to one fork and don't even bother 
+picking it up, choosing to think for the last moments of their life.
+*/
+void	*hermit_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = arg;
+	while (philo->data->alive_n_hungry)
+	{
+		think(philo);
+		time_travel(philo->data->die_time + 1);
+	}
+	return (NULL);
+}
+
+/*
+Joins the philosopher threads. 
+*/
+int	join_threads(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->seats)
+	{
+		if (pthread_join(data->philos[i].thread, NULL))
+		{
+			data->error = "joining threads failed";
+			return (KO);
+		}
+		i++;
+	}
+	return (OK);
+}
+
+/*
+Sets the simulation start_time and copies that to the each philosopher's 
+last_meal. Then each philosopher thread is started with the corresponding 
+philo struct as a parameter.
+*/
+int	start_threads(t_data *data)
+{
+	int		i;
+	t_philo	*this;
+
+	i = 0;
+	data->start_time = get_time();
+	while (i < data->seats)
+	{
+		this = &data->philos[i];
+		this->last_meal = data->start_time;
+		if (data->seats != 1 \
+			&& pthread_create(&this->thread, NULL, &philo_routine, this))
+			data->error = "multithreading failed";
+		if (data->seats == 1 \
+			&& pthread_create(&this->thread, NULL, &hermit_routine, this))
+			data->error = "multithreading failed";
+		if (data->error)
+			return (KO);
+		i++;
+	}
+	return (OK);
+}
